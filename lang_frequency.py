87169ce177617b46db4d1133baceebd721ec0c29
KhorinVitaly <khorin.v@gmail.com>
2017-07-18 12:24:46 +0600

    Учел комментарии Ильи
    Упростил подсчет частоты использования слов. Кроме того сознательно добавил две грязные функции, в которых присутствует вывод на печать, а также выбросы из программы если продолжать дальше бессмысленно.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,39 +1,43 @@
 import re
-import collections
+from collections import Counter
 from sys import argv
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, "r") as file:
-            return file.read()
-    except FileNotFoundError:
-        return None
+    with open(filepath, "r") as file:
+        return file.read()
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text, word_quantity):
     lower_text = text.lower()
     words = re.findall(r'(\w+)', lower_text, re.UNICODE)
-    statistic = collections.Counter()
-    word_quantity_in_top = 10
+    return Counter(words).most_common(word_quantity)
 
-    for word in words:
-        statistic[word] += 1
 
-    return statistic.most_common(word_quantity_in_top)
+def fetch_text():
+    try:
+        filepath = argv[1]
+        text = load_data(filepath)
+        return text
+    except FileNotFoundError:
+        print("File not found!")
+    except IndexError:
+        print("File not specified!")
+    exit()
+
+
+def print_most_frequent_words(word_quantity):
+    text = fetch_text()
+    if not text:
+        print("File is empty!")
+        exit()
+    most_frequent_words = get_most_frequent_words(text, word_quantity)
+    print("The most frequent words in your text:")
+    for word, count in most_frequent_words:
+        print(word, count)
 
 
 if __name__ == '__main__':
-    try:
-        text = load_data(argv[1])
-    except IndexError:
-        print("File not specified!")
-        exit()
+    word_quantity = 10
+    print_most_frequent_words(word_quantity)
 
-    if text:
-        most_frequent_words = get_most_frequent_words(text)
-        print("The most frequent words in your text:")
-        for word, count in most_frequent_words:
-            print(word, count)
-    else:
-        print("Text file is empty")
